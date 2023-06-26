88f6c2329f1462795e00d9cbe70e9e6c2aefe467
Otteliya2035 <otteliyadeveloper@yandex.ru>
2023-06-26 20:35:13 +0300

    feat: fix bug

--- routes/signup.js
+++ routes/signup.js
@@ -9,7 +9,7 @@ router.post('/signup', celebrate({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
     avatar: Joi.string().pattern(/^https?:\/\/(?:www\.)?[a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]+#?$/),
-    email: Joi.string().required(),
+    email: Joi.string().required().email(),
     password: Joi.string().min(8).required(),
   }),
 }), userController.createUser);
