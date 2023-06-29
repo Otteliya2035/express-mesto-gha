3c89daeee070210198b4d13c42b1b9d85e03fed0
Otteliya2035 <otteliyadeveloper@yandex.ru>
2023-06-29 13:42:02 +0300

    fix: app.js

--- middlewares/error-hendler.js
+++ middlewares/error-hendler.js
@@ -1,12 +1,16 @@
+const handleNotFound = (req, res) => {
+  res.status(404).json({ message: 'Not Found' });
+};
 const errorHandler = (err, req, res, next) => {
   if (err.statusCode) {
     res.status(err.statusCode).send({ message: err.message });
-  } else if (err.status === 404) {
-    res.status(404).json({ message: 'Not Found' });
   } else {
     res.status(500).send({ message: err.message || 'На сервере произошла ошибка' });
   }
   next();
 };
 
-module.exports = errorHandler;
+module.exports = {
+  handleNotFound,
+  errorHandler,
+};
