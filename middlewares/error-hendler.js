0a4623ee352630b38cd10559d065c95f2befb983
Otteliya2035 <otteliyadeveloper@yandex.ru>
2023-06-29 15:41:49 +0300

    fix app.js

--- middlewares/error-hendler.js
+++ middlewares/error-hendler.js
@@ -1,7 +1,5 @@
 const errorHandler = (err, req, res, next) => {
-  if (err.status === 404) {
-    res.status(404).json({ message: 'Not Found' });
-  } else if (err.statusCode) {
+  if (err.statusCode) {
     res.status(err.statusCode).send({ message: err.message });
   } else {
     res.status(500).send({ message: err.message || 'На сервере произошла ошибка' });
